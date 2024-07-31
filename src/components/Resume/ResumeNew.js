d2ae27be7213c40dbe2ecd79879f99cbf675d039
Sayak Ghorai <sayakghorai@Sayaks-MacBook-Air-M1.local>
2024-07-31 13:21:39 +0530

    Resume_update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Sayak_Ghorai_ML_Resume_NIIT_University.pdf";
+import pdf from "../../Assets/Sayak_Ghorai_Resume_ML_NIIT_University.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
